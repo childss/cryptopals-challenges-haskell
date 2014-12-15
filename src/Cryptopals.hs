bbdfe7c53c815cf900189620281983bca9d474f6
Stuart Childs <stuart@childs.io>
2014-12-15 15:25:34 -0500

    Setup failing spec for first challenge.

---
+++ src/Cryptopals.hs
@@ -0,0 +1,8 @@
+module Cryptopals where
+
+import Data.ByteString
+
+base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+base64Encode :: ByteString -> String
+base64Encode bs = []
--- test/Set1Spec.hs
+++ test/Set1Spec.hs
@@ -1,8 +1,12 @@
 module Set1Spec ( spec ) where
 
 import Test.Hspec
+import Data.ByteString.Char8 as BS
+import Data.ByteString.Base16 as Base16
+import qualified Cryptopals
 
 spec = do
-    describe "Testing" $ do
-        it "123" $ do
-            head [23 ..] `shouldBe` (23 :: Int)
+    describe "Challenge 1" $ do
+        it "convert hex to base64" $ do
+            let d = Base16.decode (BS.pack "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+            (Cryptopals.base64Encode $ fst d) `shouldBe` "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
